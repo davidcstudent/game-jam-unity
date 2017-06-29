1c6e44a10146d2d7025ff2a354cd9aa5a2cce6ba
davidcstudent <david.cagnacci@gmail.com>
2017-06-29 11:30:52 +1000

    Enemies now firing projectiles

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,7 +7,11 @@ public class Enemy : MonoBehaviour
     public float movementSpeed;
     public float minDistanceToPlayer;
     public float maxDistanceToPlayer;
+    public float maxFireRange;
+    public float fireRate;
+    public GameObject projectile;
 
+    private float fireTimer = 0;
     private GameObject target;
 
 	void Start ()
@@ -20,7 +24,6 @@ public class Enemy : MonoBehaviour
         Vector3 currentPosition = new Vector3(transform.position.x, 0, transform.position.z);
         Vector3 targetPosition = new Vector3(target.transform.position.x, 0, target.transform.position.z);
 
-
         if (Vector3.Distance(currentPosition, targetPosition) >= maxDistanceToPlayer)
         {
             transform.position = Vector3.MoveTowards(currentPosition, targetPosition, movementSpeed * Time.deltaTime);
@@ -28,6 +31,20 @@ public class Enemy : MonoBehaviour
         else if (Vector3.Distance(currentPosition, targetPosition) <= minDistanceToPlayer)
         {
             transform.position = Vector3.MoveTowards(currentPosition, targetPosition, -movementSpeed * Time.deltaTime);
+        }
+
+        if (fireTimer >= fireRate)
+        {
+            if (Vector3.Distance(currentPosition, targetPosition) < maxFireRange)
+            {
+                GameObject newProjectile = Instantiate(projectile, transform.position, Quaternion.identity);
+                newProjectile.GetComponent<Projectile>().SetDirection(targetPosition - currentPosition);
+                fireTimer = 0;
+            }
+        }
+        else
+        {
+            fireTimer += Time.deltaTime;
         }
     }
 }
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float maxLifeTime;
+    public float damage;
+    public float speed;
+
+    private float currentLifeTime = 0;
+    private Vector3 direction = new Vector3(0, 0, 0);
+
+	void Start ()
+    {
+		
+	}
+
+	void Update ()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+
+        currentLifeTime += Time.deltaTime;
+
+        if (currentLifeTime >= maxLifeTime)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    public void SetDirection(Vector3 direction)
+    {
+        this.direction = direction.normalized;
+    }
+}
