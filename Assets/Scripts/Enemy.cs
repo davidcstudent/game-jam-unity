7373066635a6259fc035ce721c4922a2a06fb491
davidcstudent <david.cagnacci@gmail.com>
2017-06-29 12:34:18 +1000

    Enemies collide with each other

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -12,11 +12,13 @@ public class Enemy : MonoBehaviour
     private GameObject target;
     private Weapon weapon;
     private bool isDead;
+    private CharacterController controller;
 
 	void Start ()
     {
         target = GameObject.FindGameObjectWithTag("Player");
         weapon = GetComponent<Weapon>();
+        controller = GetComponent<CharacterController>();
 	}
 	
 	void Update ()
@@ -24,13 +26,17 @@ public class Enemy : MonoBehaviour
         Vector3 currentPosition = new Vector3(transform.position.x, 0, transform.position.z);
         Vector3 targetPosition = new Vector3(target.transform.position.x, 0, target.transform.position.z);
 
+        Vector3 direction = (targetPosition - currentPosition).normalized;
+
         if (Vector3.Distance(currentPosition, targetPosition) >= maxDistanceToPlayer)
         {
-            transform.position = Vector3.MoveTowards(currentPosition, targetPosition, movementSpeed * Time.deltaTime);
+            controller.Move(direction * movementSpeed * Time.deltaTime);
+            //transform.position = Vector3.MoveTowards(currentPosition, targetPosition, movementSpeed * Time.deltaTime);
         }
         else if (Vector3.Distance(currentPosition, targetPosition) <= minDistanceToPlayer)
         {
-            transform.position = Vector3.MoveTowards(currentPosition, targetPosition, -movementSpeed * Time.deltaTime);
+            controller.Move(direction * -movementSpeed * Time.deltaTime);
+            //transform.position = Vector3.MoveTowards(currentPosition, targetPosition, -movementSpeed * Time.deltaTime);
         }
 
         if (Vector3.Distance(currentPosition, targetPosition) < maxFireRange)
