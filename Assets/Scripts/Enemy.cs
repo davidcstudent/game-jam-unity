e1e8461ae5ed472a7e19a445c40bd9449d4b6e7e
davidcstudent <david.cagnacci@gmail.com>
2017-06-29 10:18:03 +1000

    Added project to repository

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float movementSpeed;
+
+    private GameObject target;
+
+	void Start ()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	void Update ()
+    {
+        Vector3 targetPosition = target.transform.position;
+
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, movementSpeed * Time.deltaTime);
+	}
+}
