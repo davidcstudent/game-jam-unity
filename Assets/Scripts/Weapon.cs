7ff3634e3e2afea2218dfde1fb43502b7e98ab47
davidcstudent <david.cagnacci@gmail.com>
2017-06-29 14:44:28 +1000

    Weapons firing from fire point on model

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -6,6 +6,7 @@ public class Weapon : MonoBehaviour
 {
     public float fireRate;
     public GameObject projectile;
+    public Transform firePoint;
 
     private float fireTimer;
 	
@@ -21,7 +22,7 @@ public class Weapon : MonoBehaviour
     {
         if (fireTimer >= fireRate)
         {
-            GameObject newProjectile = Instantiate(projectile, transform.position, Quaternion.identity);
+            GameObject newProjectile = Instantiate(projectile, firePoint.position, Quaternion.identity);
             newProjectile.GetComponent<Projectile>().SetVelocity(direction);
             Physics.IgnoreCollision(newProjectile.GetComponent<Collider>(), gameObject.GetComponent<Collider>());
             fireTimer = 0;
